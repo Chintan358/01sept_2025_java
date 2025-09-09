4c5e3fd5ff131a61bf2cd6dae8c5c8ff8d5c8d49
chintan <chintan@gmail.com>
2025-09-09 13:58:45 +0530

    all

---
+++ Learning_Corejava/src/basic/Demo.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+//		System.out.println("Ok");
+		
+		int a = 10;
+		String name = "Hello";
+		String fname = "Test";
+		
+		
+		
+		
+		
+	}
+}
