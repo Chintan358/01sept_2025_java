7384b79734bfa04c8ef759d30c3bca266e5f8a8f
chintan <chintan@gmail.com>
2025-12-25 14:46:20 +0530

    all

--- MVC_CRUD/src/main/java/dao/StudentDao.java
+++ MVC_CRUD/src/main/java/dao/StudentDao.java
@@ -3,7 +3,9 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Student;
 
@@ -40,4 +42,50 @@ public class StudentDao {
 		}
 		return i;
 	}
+	
+	
+	public ArrayList<Student> viewStudents()
+	{
+		ArrayList<Student> al = new ArrayList<Student>();
+		try {
+			PreparedStatement ps = 
+					cn.prepareStatement("select * from student");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				Student st = new Student();
+				st.setId(rs.getInt(1));
+				st.setName(rs.getString(2));
+				st.setEmail(rs.getString(3));
+				st.setPhone(rs.getString(4));
+				al.add(st);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return al;
+		
+	}
+	
+	
+	
+	
+	public int deleteStudent(int id)
+	{
+		int i=0;
+		try {
+			PreparedStatement ps = cn.prepareStatement("delete from student where id=?");
+			ps.setInt(1, id);
+			i  =ps.executeUpdate();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return i;
+	}
+	
 }
--- MVC_CRUD/src/main/webapp/index.jsp
+++ MVC_CRUD/src/main/webapp/index.jsp
@@ -5,17 +5,31 @@
 <head>
 <meta charset="UTF-8">
 <title>Home</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
 
+		<div class="container">
+			<div class="row">
+				<div class="col-5 mx-auto mt-3 p-5 card">
+				<h2>Student Registration</h2>
+				<hr>
 		${msg}
 		<form action="reg" method="post">
-		<input type="text" name="name" placeholder="Enter name">
-		<input type="text" name="email" placeholder="Enter eamil">
-		<input type="text" name="phone" placeholder="Enter phone">
-
-		<input type="submit">		
+		<input type="text" name="name" placeholder="Enter name" class="form-control">
+		<br>
+		<input type="text" name="email" placeholder="Enter eamil" class="form-control">
+		<br>
+		<input type="text" name="phone" placeholder="Enter phone" class="form-control">
+<br>
+		<input type="submit" class="btn btn-success">	
+		<input type="reset" class="btn btn-primary">
+		<a href="display" class="btn btn-warning">Display</a>	
 		</form>
+		
+		</div>
+			</div>
+		</div>
 
 </body>
 </html>
