59d99f7946cad7529d0225c06b5a69d9273fec87
chintan <chintan@gmail.com>
2026-02-10 15:25:50 +0530

    all

---
+++ A004_Springboot-1/src/main/java/com/example/demo/A004Springboot1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A004Springboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A004Springboot1Application.class, args);
+	}
+
+}
---
+++ A004_Springboot/src/main/java/com/example/demo/A004SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A004SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A004SpringbootApplication.class, args);
+	}
+
+}
