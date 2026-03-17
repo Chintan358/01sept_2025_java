a133a18fbd37fd62face0d87ddf58ac238f644a8
chintan <chintan@gmail.com>
2026-03-17 15:22:34 +0530

    all

--- A004_Springboot/src/main/java/com/example/demo/controller/HelloController.java
+++ A004_Springboot/src/main/java/com/example/demo/controller/HelloController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 	
-	@GetMapping("/")
+	@GetMapping("/admin")
 	public String index()
 	{
 		return "index";
+	}
+	
+	
+	@GetMapping("/user")
+	public String home()
+	{
+		return "home";
 	}
 }
