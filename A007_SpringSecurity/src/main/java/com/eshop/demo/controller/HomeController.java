3be21fc889b254cd22078570e049966a135b8df3
chintan <chintan@gmail.com>
2026-03-18 15:40:58 +0530

    all

---
+++ A007_SpringSecurity/src/main/java/com/eshop/demo/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.eshop.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+		@GetMapping("/user")
+		public String index()
+		{
+			return "index";
+		}
+		
+		@GetMapping("/admin")
+		public String admin()
+		{
+			return "Admin";
+		}
+		
+		@GetMapping("/login")
+		public String login()
+		{
+			return "login";
+		}
+		
+		@GetMapping("/fail")
+		public String fail()
+		{
+			return "fail";
+		}
+		
+		@GetMapping("/home")
+		public String home()
+		{
+			return "home";
+		}
+		
+		@GetMapping("/logout")
+		public String logout()
+		{
+			return "logout";
+		}
+}
